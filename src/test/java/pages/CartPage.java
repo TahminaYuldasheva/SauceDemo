50dded3b68d8bb0352164fd7b6b279e73b8e6a45
Tahmina Yuldasheva <tahayuldasheva@gmail.com>
2025-06-10 19:04:11 +0200

    All methods in pages are logged

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,9 +1,11 @@
 package pages;
 
 import io.qameta.allure.Step;
+import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
+@Log4j2
 public class CartPage extends BasePage {
 
     private static final By TITLE = By.xpath("//*[@data-test='title']");
@@ -17,21 +19,25 @@ public class CartPage extends BasePage {
     }
 
     public String getTitle() {
-        return driver.findElement(TITLE).getText();
+        String title = driver.findElement(TITLE).getText();
+        log.info("Getting title text: {}", title);
+        return title;
     }
 
     @Step("Нажатие на кнопку Checkout")
     public void clickCheckoutButton() {
+        log.info("Clicking on Checkout button");
         driver.findElement(CHECKOUT_ID).click();
     }
 
     public boolean isCartNotEmpty() {
+        log.info("Checking if cart is not empty");
         return driver.findElement(CART_ITEM).isDisplayed();
     }
 
     @Step("Нажатие на кнопку Continue Shopping")
     public void clickContinueShoppingButton() {
+        log.info("Clicking Continue Shopping button");
         driver.findElement(CONTINUE_SHOPPING).click();
     }
-
 }
